7edbb6d81dae8b47d5de7034cf4629fc644b2e89
ifshuaishuai <ifshuaishuai@gmail.com>
2020-09-06 17:40:38 +0800

    [Fix] Path finding spell direction

--- Assets/Sources/_Systems/Logic/Systems/Pathfinding/StopPathfindingSystem.cs
+++ Assets/Sources/_Systems/Logic/Systems/Pathfinding/StopPathfindingSystem.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 
 public class StopPathfindingSystem : ReactiveSystem<GameEntity>
 {
+    private GameContext _gameContext;
+
+    private IGroup<GameEntity> _underControlGroup;
+    private IGroup<GameEntity> _aiGroup;
+    private readonly List<GameEntity> _cleanBuffer = new List<GameEntity>();
+    
     public StopPathfindingSystem(Contexts contexts)
         : base(contexts.game)
     {
-        
+        _gameContext = contexts.game;
+
+        _underControlGroup = _gameContext.GetGroup(GameMatcher.UnderControl);
+        _aiGroup = _gameContext.GetGroup(GameMatcher.AI);
     }
     
     protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
@@ -26,6 +35,61 @@ public class StopPathfindingSystem : ReactiveSystem<GameEntity>
         foreach (var e in entities)
         {
             e.pathfinding.pathfinding.Cleanup();
+
+            SetupTargetEnemyInternal(e);
         }
+    }
+
+    private void SetupTargetEnemyInternal(GameEntity entity)
+    {
+        var underControlEntity = entity;
+        GameEntity enemy = FindNearestEnemy(underControlEntity);
+
+        if (enemy != null)
+        {
+            underControlEntity.ReplaceTarget(enemy);
+
+            // direction
+            float srcX = underControlEntity.position.x;
+            float destX = enemy.position.x;
+
+            underControlEntity.ReplaceDirection(destX >= srcX ? CharacterDirection.Right : CharacterDirection.Left);
+        }
+    }
+    
+    private GameEntity FindNearestEnemy(GameEntity underControlEntity)
+    {
+        var entities = _aiGroup.GetEntities(_cleanBuffer);
+
+        float minDistance = float.MaxValue;
+        GameEntity enemy = null;
+
+        Vector2 srcPos = new Vector2(underControlEntity.position.x, underControlEntity.position.y);
+        
+        foreach (var e in entities)
+        {
+            if (e.aI.type == AIType.Enemy)
+            {
+                Vector2 destPos = new Vector2(e.position.x, e.position.y);
+
+                float distance = (destPos - srcPos).sqrMagnitude;
+
+                if (distance < minDistance)
+                {
+                    enemy = e;
+                    minDistance = distance;
+                }
+                else
+                {
+                    // nothing
+                }
+            }
+            else
+            {
+                // nothing
+            }
+        }
+
+        return enemy;
     }
 }
