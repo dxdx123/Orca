31451e144ee1db993efe8b5873daae6d3d6814f9
ifshuaishuai <ifshuaishuai@gmail.com>
2020-09-06 17:46:02 +0800

    [Cleanup] Copied code

--- Assets/Sources/_Systems/Logic/Systems/Pathfinding/StopPathfindingSystem.cs
+++ Assets/Sources/_Systems/Logic/Systems/Pathfinding/StopPathfindingSystem.cs
@@ -5,19 +5,9 @@ using UnityEngine;
 
 public class StopPathfindingSystem : ReactiveSystem<GameEntity>
 {
-    private GameContext _gameContext;
-
-    private IGroup<GameEntity> _underControlGroup;
-    private IGroup<GameEntity> _aiGroup;
-    private readonly List<GameEntity> _cleanBuffer = new List<GameEntity>();
-    
     public StopPathfindingSystem(Contexts contexts)
         : base(contexts.game)
     {
-        _gameContext = contexts.game;
-
-        _underControlGroup = _gameContext.GetGroup(GameMatcher.UnderControl);
-        _aiGroup = _gameContext.GetGroup(GameMatcher.AI);
     }
     
     protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
@@ -43,7 +33,7 @@ public class StopPathfindingSystem : ReactiveSystem<GameEntity>
     private void SetupTargetEnemyInternal(GameEntity entity)
     {
         var underControlEntity = entity;
-        GameEntity enemy = FindNearestEnemy(underControlEntity);
+        GameEntity enemy = entity.target.target;
 
         if (enemy != null)
         {
@@ -55,41 +45,5 @@ public class StopPathfindingSystem : ReactiveSystem<GameEntity>
 
             underControlEntity.ReplaceDirection(destX >= srcX ? CharacterDirection.Right : CharacterDirection.Left);
         }
-    }
-    
-    private GameEntity FindNearestEnemy(GameEntity underControlEntity)
-    {
-        var entities = _aiGroup.GetEntities(_cleanBuffer);
-
-        float minDistance = float.MaxValue;
-        GameEntity enemy = null;
-
-        Vector2 srcPos = new Vector2(underControlEntity.position.x, underControlEntity.position.y);
-        
-        foreach (var e in entities)
-        {
-            if (e.aI.type == AIType.Enemy)
-            {
-                Vector2 destPos = new Vector2(e.position.x, e.position.y);
-
-                float distance = (destPos - srcPos).sqrMagnitude;
-
-                if (distance < minDistance)
-                {
-                    enemy = e;
-                    minDistance = distance;
-                }
-                else
-                {
-                    // nothing
-                }
-            }
-            else
-            {
-                // nothing
-            }
-        }
-
-        return enemy;
     }
 }
